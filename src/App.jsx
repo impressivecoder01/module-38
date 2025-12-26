917959fee448a39d30767366ff337020b8bb933a
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-27 01:40:29 +0600

    data loaded class two

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries/Countries'
-
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+.then(res => res.json())
 function App() {
  
 
   return (
     <>
-      <Countries></Countries>
+    <Suspense fallback={<p>Coming soon</p>}>
+      <Countries countriesPromise={countriesPromise}></Countries>
+    </Suspense>
     </>
   )
 }
