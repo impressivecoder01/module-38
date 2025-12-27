ed0fb3db4d34cddfb66661ceca0ea9fc321faec7
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-27 22:40:28 +0600

    class seven

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -8,11 +8,18 @@ const Countries = ({ countriesPromise }) => {
     const [visitedCountries, setVisitedCountries] = useState([])
     const handleVisitedCountries = (country)=> {
         console.log('coutries',country)
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries)
     }
     return (
         <div >
             <h1>Countries: {countries.length}</h1>
-            <h1>Total country visited: </h1>
+            <h1>Total country visited: {visitedCountries.length} </h1>
+            <ol>
+                {
+                    visitedCountries.map(country => <li key={country.cca3} >{country.name.common} </li>)
+                }
+            </ol>
             <div className='countries'>
                 {
                     countries.map(country => <Country handleVisitedCountries={handleVisitedCountries} key={country.cca3.cca3} country={country}></Country>)
